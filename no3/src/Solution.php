e0c77ace0a303b994673ed0ea78bc11066de6557
Ian Chiang <ianc@motom.me>
2021-08-02 10:22:50 +0800

    init

---
+++ no3/src/Solution.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace no3\src;
+class Solution {
+
+    /**
+     * @param String $s
+     * @return String
+     */
+    function longestPalindrome($s): string
+    {
+
+        return '';
+    }
+}
---
+++ no3/tests/SolutionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace no3\tests;
+use no3\src\Solution;
+use PHPUnit\Framework\TestCase;
+
+class SolutionTest extends TestCase
+{
+
+    private $solution;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->solution = new Solution();
+    }
+
+    public function testLongestPalindrome1()
+    {
+        $this->assertEquals($this->solution->longestPalindrome('babad'),'bab');
+    }
+
+    public function testLongestPalindrome2()
+    {
+        $this->assertEquals($this->solution->longestPalindrome('cbbd'),'bb');
+    }
+
+    public function testLongestPalindrome3()
+    {
+        $this->assertEquals($this->solution->longestPalindrome('a'),'a');
+    }
+
+    public function testLongestPalindrome4()
+    {
+        $this->assertEquals($this->solution->longestPalindrome('ac'),'a');
+    }
+}
