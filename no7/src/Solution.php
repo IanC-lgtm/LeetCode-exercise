b75bf440ba8c296ade7f67b37318aa4a9464082a
Ian Chiang <ianc@motom.me>
2021-08-03 22:59:49 +0800

    q7
    - finish with solution

--- no7/src/Solution.php
+++ no7/src/Solution.php
@@ -4,45 +4,23 @@ namespace no7\src;
 class Solution
 {
 
-
     /**
      * @param String $s
      * @return Boolean
      */
     function isValid($s)
     {
-        return $this->test(str_split($s));
-    }
-
-    private function test(array $s)
-    {
-        $checked = [$s[0]];
-        $lastIndex = 0;
-        $blankCount = ($s[0] === '') ? 1 : 0;
-
-        for ($i = 1; $i < count($s); $i++) {
-
-            if ($s[$i] === '') {
-                ++$blankCount;
-            } else if (($s[$i] === ']' && array_pop($checked) === '[') ||
-                ($s[$i] === ')' && array_pop($checked) === '(') ||
-                ($s[$i] === '}' && array_pop($checked) === '{')
-            ) {
-                $s[$i] = '';
-                $s[$lastIndex] = '';
-                return $this->test($s);
-            } else if ($s[$i] !== '') {
-                $checked[] = $s[$i];
-                $lastIndex = $i;
+        $eg = ["{" => "}", "[" => "]", "(" => ")"];
+        $stack = [];
+        for ($i = 0; $i < strlen($s); $i++) {
+            $tmp = end($stack);
+            if ($tmp !== false && $eg["{$tmp}"] == $s[$i]) {
+                unset($stack[key($stack)]);
+            } else {
+                $stack[] = $s[$i];
             }
-
         }
-
-        if ($blankCount === count($s)) {
-            return true;
-        }
-
-        return false;
-
+        return count($stack) == 0;
     }
+
 }
