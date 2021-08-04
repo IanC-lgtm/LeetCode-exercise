b11f9cc33405330333fccf1654654aa7066b5bc2
Ian Chiang <ianc@motom.me>
2021-08-04 18:19:55 +0800

    q8
    - add basic function˚

--- no8/tests/SolutionTest.php
+++ no8/tests/SolutionTest.php
@@ -2,6 +2,7 @@
 
 namespace no8\tests;
 
+use no8\src\ListNode;
 use no8\src\Solution;
 use PHPUnit\Framework\TestCase;
 
@@ -14,6 +15,26 @@ class SolutionTest extends TestCase
     {
         parent::setUp();
         $this->solution = new Solution();
+    }
+
+
+    private function arrayToListAndGetFirstNode(array $nums): ListNode
+    {
+
+        $firstNode = new ListNode(
+            $nums[0],
+            (count($nums) === 1) ? null : $this->getNextNode(1, $nums));
+
+        return $firstNode;
+
+    }
+
+    private function getNextNode(int $currentIndex, array $nums): ListNode
+    {
+        if ($currentIndex >= count($nums) - 1) {
+            return new ListNode($nums[$currentIndex], null);
+        }
+        return new ListNode($nums[$currentIndex], $this->getNextNode($currentIndex + 1, $nums));
     }
 
 
