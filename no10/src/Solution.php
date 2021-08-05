ec66ee76b27ab394836ca9215baeac5fca9a2bab
Ian Chiang <ianc@motom.me>
2021-08-05 10:42:18 +0800

    q10
    -init

---
+++ no10/src/Solution.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace no10\src;
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer
+     */
+    function search($nums, $target)
+    {
+
+    }
+}
---
+++ no10/tests/SolutionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace no10\tests;
+
+use no10\src\Solution;
+use PHPUnit\Framework\TestCase;
+
+class SolutionTest extends TestCase
+{
+
+    private $solution;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->solution = new Solution();
+    }
+
+    public function testSearch1()
+    {
+        $this->assertEquals(4, $this->solution->search([4, 5, 6, 7, 0, 1, 2], 0));
+    }
+
+    public function testSearch2()
+    {
+        $this->assertEquals(-1, $this->solution->search([4, 5, 6, 7, 0, 1, 2], 3));
+    }
+
+    public function testSearch3()
+    {
+        $this->assertEquals(-1, $this->solution->search([1], 0));
+    }
+
+}
