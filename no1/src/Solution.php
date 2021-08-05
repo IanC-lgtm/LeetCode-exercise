ba583b0e3d9921be7cc712b81d4cdb2bc681d283
Ian Chiang <ianc@motom.me>
2021-08-05 14:28:48 +0800

    q1
    - one pass solution

--- no1/src/Solution.php
+++ no1/src/Solution.php
@@ -4,4 +4,31 @@ namespace no1\src;
 class Solution
 {
 
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target)
+    {
+
+        $checked = [];
+
+        for ($i = 0; $i < count($nums); $i++) {
+
+            $targetValue = $target - $nums[$i];
+
+            if (
+                isset($checked[$nums[$i]])
+            ) {
+                return [$checked[$nums[$i]], $i];
+            } else {
+                $checked[$targetValue] = $i;
+            }
+
+
+        }
+
+    }
+
 }
--- no1/tests/SolutionTest.php
+++ no1/tests/SolutionTest.php
@@ -16,5 +16,27 @@ class SolutionTest extends TestCase
         $this->solution = new Solution();
     }
 
+    public function testTwoSum1()
+    {
+
+        $this->assertEquals([0, 1], $this->solution->twoSum([2, 7, 11, 15], 9));
+
+    }
+
+    public function testTwoSum2()
+    {
+
+        $this->assertEquals([1, 2], $this->solution->twoSum([3, 2, 4], 6));
+
+    }
+
+
+    public function testTwoSum3()
+    {
+
+        $this->assertEquals([0, 1], $this->solution->twoSum([3, 3], 6));
+
+    }
+
 
 }
