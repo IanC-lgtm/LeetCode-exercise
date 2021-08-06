c028533a003fc554035b7b4e2f93ee7c7dbc6f95
Ian Chiang <ianc@motom.me>
2021-08-06 10:51:25 +0800

    q11
    - with recursive solution

--- no11/src/Solution.php
+++ no11/src/Solution.php
@@ -4,6 +4,9 @@ namespace no11\src;
 class Solution
 {
 
+    private $results = [];
+    private $candidates = [];
+
     /**
      * @param Integer[] $candidates
      * @param Integer $target
@@ -12,6 +15,37 @@ class Solution
     function combinationSum($candidates, $target)
     {
 
+        $this->candidates = $candidates;
+
+        $this->sum(0, $target, []);
+
+        return $this->results;
+
     }
-    
+
+    function sum($i, $target, $currentResult = [])
+    {
+        if ($target === 0) {
+            // get result
+            $this->results[] = $currentResult;
+            return;
+        }
+
+        if ($i >= count($this->candidates) || $target < 0) {
+            // no need processed
+            return;
+        }
+
+        $currentResult[] = $this->candidates[$i];
+
+        // keep check current
+        $this->sum($i, $target - $this->candidates[$i], $currentResult);
+
+        array_pop($currentResult);
+
+        // next
+        $this->sum($i + 1, $target, $currentResult);
+
+    }
+
 }
