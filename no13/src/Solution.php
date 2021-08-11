e87779e63a6222b71259a0f69af126811975e2d6
Ian Chiang <ianc@motom.me>
2021-08-11 21:07:28 +0800

    q13
    - init

---
+++ no13/src/Solution.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace no13\src;
+class Solution
+{
+    /**
+     * @param String[] $strs
+     * @return String[][]
+     */
+    function groupAnagrams($strs) {
+
+    }
+}
---
+++ no13/tests/SolutionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace no13\tests;
+
+use no13\src\Solution;
+use PHPUnit\Framework\TestCase;
+
+class SolutionTest extends TestCase
+{
+
+    private $solution;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->solution = new Solution();
+    }
+
+    public function test1(){
+
+        $this->assertEquals([["bat"],["nat","tan"],["ate","eat","tea"]],
+            $this->solution->groupAnagrams(["eat","tea","tan","ate","nat","bat"]));
+
+    }
+
+
+    public function test2(){
+
+        $this->assertEquals([[""]],
+            $this->solution->groupAnagrams([""]));
+
+    }
+
+    public function test3(){
+
+        $this->assertEquals([["a"]],
+            $this->solution->groupAnagrams(["a"]));
+
+    }
+}
