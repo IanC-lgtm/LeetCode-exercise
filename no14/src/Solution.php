accb2ca9083c8b577cdd49730f04f40f863d517c
Ian Chiang <ianc@motom.me>
2021-08-12 22:12:34 +0800

    q14
    - finish with basic method

--- no14/src/Solution.php
+++ no14/src/Solution.php
@@ -9,5 +9,21 @@ class Solution
      */
     function maxSubArray($nums) {
 
+        $max=$nums[0];
+        $currentSum=max($max,0);
+
+        for ($i=1 ; $i<count($nums);$i++) {
+
+            $currentSum+=$nums[$i];
+
+            $max= max($max, $currentSum);
+
+            // reset to 0
+            $currentSum=($currentSum<0)?0:$currentSum;
+
+        }
+
+        return $max;
+
     }
 }
