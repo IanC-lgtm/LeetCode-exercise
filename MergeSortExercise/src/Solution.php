016f2fef1d7c71d4c8e56e80a0e7ab42d434c2c0
Ian Chiang <ianc@motom.me>
2021-08-13 21:54:47 +0800

    MergeSortExercise
    - finish

--- MergeSortExercise/src/Solution.php
+++ MergeSortExercise/src/Solution.php
@@ -4,4 +4,53 @@ namespace MergeSortExercise\src;
 class Solution
 {
 
+    public function sort(array $nums){
+
+        $result = $this->startSort($nums, 0, count($nums) - 1);
+        return $result;
+
+    }
+
+    private function startSort(array $nums, int $start, int $end):array
+    {
+        echo json_encode($nums).PHP_EOL;
+
+        $count = count($nums);
+        if($count <=2){
+            switch ($count){
+                case  0:
+                    return [];
+                case  1:
+                    return [$nums[0]];
+                case  2:
+                    return ($nums[0] > $nums[1])?[$nums[1],$nums[0] ]:[$nums[0],$nums[1]];
+            }
+
+        }
+
+        $mid=(int)floor(($end-$start) / 2);
+        echo $mid.PHP_EOL;
+
+        // divide
+        $leftArray=$this->startSort(array_slice($nums,$start, $mid-$start+1),$start,$mid);
+        $rightArray=$this->startSort(array_slice($nums,$mid+1,$end-$mid),$start,$mid);
+
+
+        // merge these divide array
+        $result=[];
+
+        while(count($leftArray) && count($rightArray)){
+            if($leftArray[0]>$rightArray[0]){
+                $result[]=array_shift($rightArray);
+
+            }else{
+                $result[]=array_shift($leftArray);
+
+            }
+
+        }
+
+        return array_merge($result,$leftArray,$rightArray);
+
+    }
 }
--- MergeSortExercise/tests/SolutionTest.php
+++ MergeSortExercise/tests/SolutionTest.php
@@ -18,7 +18,20 @@ class SolutionTest extends TestCase
 
     public function test1(){
 
-        $this->assertEquals('',$this->solution);
+        $this->assertEquals([1,2,3,5],$this->solution->sort([1,5,3,2]));
+
+    }
+
+
+    public function test2(){
+
+        $this->assertEquals([3,5,9,21,32,34,35,37,50,55,64,70,82,89],$this->solution->sort([21, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, 70]));
+
+    }
+
+    public function test3(){
+
+        $this->assertEquals([1,2,4,6,7,9],$this->solution->sort([1,6,4,9,7,2]));
 
     }
 
