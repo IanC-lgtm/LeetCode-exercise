0dfae443661fa6425815765a909702266aa3adf7
Ian Chiang <ianc@motom.me>
2021-08-14 20:24:37 +0800

    q10-1-x
    - finish by referring last correct answer

--- no10/src/Solution.php
+++ no10/src/Solution.php
@@ -9,39 +9,52 @@ class Solution
      * @param Integer $target
      * @return Integer
      */
-    function search($nums, $target)
-    {
+    function search($nums, $target) {
 
-        $left = 0;
-        $right = count($nums) - 1;
 
-        while ($left <= $right) {
+        $left=0;
+        $right=count($nums)-1;
 
-            $mid = $left + (int)floor(($right - $left) / 2);
+        while($right>=$left){
 
-            if ($nums[$mid] === $target) {
+            $mid=(int)floor(($right+$left)/2);
+            $midNum=$nums[$mid];
+
+            if($midNum===$target){
                 return $mid;
             }
 
-            if ($nums[$mid] < $nums[$right]) {
-                //right is sorted, check right hand
-                if ($target > $nums[$mid] && $nums[$right] >= $target) {
-                    $left = $mid + 1;
-                } else {
-                    $right = $mid - 1;
+            $leftNum=$nums[$left];
+            $rightNum=$nums[$right];
+
+            if($midNum<$rightNum){
+                //right is sort
+
+                if($target > $midNum && $rightNum>=$target){
+                    // making sure the target is in right hand
+                    $left=$mid+1;
+                }else{
+                    $right=$mid-1;
                 }
-            } else if ($nums[$mid] >= $nums[$right]) {
-                //left is sorted
-                if ($target < $nums[$mid] && $nums[$left] <= $target) {
-                    $right = $mid - 1;
-                } else {
-                    $left = $mid + 1;
+
+
+            }else if($midNum>=$rightNum){
+                // left is sort
+
+                if($target < $midNum && $leftNum<=$target){
+                    // target is in left hand
+                    $right=$mid-1;
+                }else{
+                    $left=$mid+1;
                 }
+
             }
+
 
         }
 
         return -1;
 
     }
+
 }
