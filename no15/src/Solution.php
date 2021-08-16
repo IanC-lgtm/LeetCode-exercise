19a73c9bc70cc6806d349faa719ba9ddb7e83291
Ian Chiang <ianc@motom.me>
2021-08-16 22:12:21 +0800

    q15
    - finish testcase 1

--- no15/src/Solution.php
+++ no15/src/Solution.php
@@ -10,5 +10,48 @@ class Solution
      */
     function spiralOrder($matrix) {
 
+        $endX=(int)floor(count($matrix[0])/2);
+        $endY=(int)floor(count($matrix)/2);
+
+        $currentX=0;
+        $currentY=0;
+
+        $result=[];
+        $iteration=0;
+        while( ! ($currentX===$endX && $currentY=== $endY)){
+
+            for ($x1=$iteration ; $x1<count($matrix[0])-$iteration;$x1++) {
+                $result[]=$matrix[$iteration][$x1];
+                $currentX=$x1;
+            }
+
+            --$x1;
+
+            for ($y1=$iteration+1 ; $y1<count($matrix)-$iteration;$y1++) {
+                $result[]=$matrix[$y1][$x1];
+                $currentY=$y1;
+            }
+
+            --$y1;
+
+            for ($x2=$x1-1 ; $x2>$iteration-1;$x2--) {
+                $result[]=$matrix[$y1][$x2];
+                $currentX=$x2;
+            }
+            --$x2;
+
+            for ($y2=$y1-1 ; $y2>$iteration;$y2--) {
+                $result[]=$matrix[$y2][$iteration];
+                $currentY=$y2;
+            }
+
+            $iteration++;
+
+            var_dump($result);
+
+        }
+
+        return $result;
+
     }
 }
--- no15/tests/SolutionTest.php
+++ no15/tests/SolutionTest.php
@@ -23,4 +23,9 @@ class SolutionTest extends TestCase
     }
 
 
+    public function test2(){
+
+        $this->assertEquals([1,2,3,4,8,12,11,10,9,5,6,7],$this->solution->spiralOrder( [[1,2,3,4],[5,6,7,8],[9,10,11,12]]));
+
+    }
 }
