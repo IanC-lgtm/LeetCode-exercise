28d2afc5eb628f7175caa1e72619182173cd1cf6
Ian Chiang <ianc@motom.me>
2021-08-19 20:19:39 +0800

    q17
    - with solution video's solution

--- no17/src/Solution.php
+++ no17/src/Solution.php
@@ -14,59 +14,32 @@ class Solution
            return [];
         }
 
-        $start=0;
-        $end=count($intervals)-1;
+        sort($intervals);
 
-        $results=$this->startMerge($intervals,$start,$end);
+        $results=[];
+
+        for ($i=0 ; $i<=count($intervals)-1;$i++) {
+
+            $currentInterval = $intervals[$i];
+
+            $lastIndex = count($results) - 1;
+
+            if($lastIndex===-1 || $results[$lastIndex][1] < $currentInterval[0]){
+                $results[]= $currentInterval;
+            }else{
+
+                if($results[$lastIndex][1] >= $currentInterval[0]){
+                    $results[$lastIndex][0]=min($currentInterval[0],$results[$lastIndex][0]);
+                    $results[$lastIndex][1]=max($currentInterval[1],$results[$lastIndex][1]);
+                }
+
+            }
+
+
+        }
 
         return $results;
 
     }
 
-    private function startMerge(array $intervals, int $start, int $end, $s='start')
-    {
-        echo $s.':'. json_encode($intervals).PHP_EOL;
-        if(count($intervals)<=1){
-            return $intervals;
-        }
-
-        $mid=(int)floor(($end-$start) /2 );
-
-        $left=$this->startMerge( array_slice($intervals,$start,$mid-$start+1),0,$mid,'left');
-        $right=$this->startMerge(array_slice($intervals,$mid+1,$end-$mid),0,$mid,'right');
-
-
-        $result=[];
-        while(count($left) && count($right) ){
-            $leftT=$left[0];
-            $rightT=$right[0];
-
-            $l1=$leftT[0];
-            $l2=$leftT[1];
-            $r1=$rightT[0];
-            $r2=$rightT[1];
-
-            if(
-                ($l2>=$r1 && $l1<=$r1) ||
-                ($l2>=$r2 && $l1<=$r2) ||
-                // all coverage
-                ($l1 <= $r1 && $l2>=$r2) ||
-                ($r1 <= $l1 && $r2 >= $l2)
-            ){
-                //head include
-                //end include
-                $result[]=[min($l1,$r1),max($l2,$r2)];
-            }else{
-                $result[]=$leftT;
-                $result[]=$rightT;
-            }
-            array_shift($left);
-            array_shift($right);
-        }
-
-
-        return array_merge($result,$left,$right);
-
-
-    }
 }
