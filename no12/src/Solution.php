77ea1e2c9007e754f3adbaeb88effbdfc5ed9c96
Ian Chiang <i29686112@gmail.com>
2021-08-22 21:27:59 +0800

    q12
    - with transpose + reflect

--- no12/src/Solution.php
+++ no12/src/Solution.php
@@ -9,40 +9,42 @@ class Solution
      * @param Integer[][] $matrix
      * @return NULL
      */
-    function rotate(&$matrix) {
+    function rotate(&$matrix)
+    {
 
-        $n = count($matrix[0]);
 
-        // do matrix transpose
-        $this->transpose($n, $matrix);
-        $this->reflect($n, $matrix);
+        $this->transpose($matrix);
 
+        $this->reflect($matrix);
 
     }
 
-
-    protected function transpose(int $n, array &$matrix)
+    private function transpose(array &$matrix)
     {
-        for ($i = 0; $i < $n; $i++) {
-            for ($j = $i; $j < $n; $j++) {
+        for ($i = 0; $i < count($matrix); $i++) {
+            for ($j = $i; $j < count($matrix); $j++) {
                 $temp = $matrix[$j][$i];
                 $matrix[$j][$i] = $matrix[$i][$j];
                 $matrix[$i][$j] = $temp;
-            }
 
+            }
         }
+
     }
 
-    private function reflect(int $n, &$matrix)
+    private function reflect(array &$matrix)
     {
-        for ($i = 0; $i < $n; $i++) {
-            for ($j = 0; $j < floor($n/2); $j++) {
-                $temp = $matrix[$i][$j];
-                $matrix[$i][$j] = $matrix[$i][$n-$j-1];
-                $matrix[$i][$n-$j-1] = $temp;
-            }
+        $n = count($matrix);
+        for ($i = 0; $i < floor($n / 2); $i++) {
+            for ($j = 0; $j < $n; $j++) {
 
+                $temp = $matrix[$j][$i];
+                $matrix[$j][$i] = $matrix[$j][$n - $i - 1];
+                $matrix[$j][$n - $i - 1] = $temp;
+
+            }
         }
+
     }
 
 
