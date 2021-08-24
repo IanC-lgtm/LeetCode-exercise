813c3e37aa0841f90a9999f849e72b9c48abe3d1
Ian Chiang <ianc@motom.me>
2021-08-24 17:34:06 +0800

    q18
    - finish with https://leetcode.com/problems/insert-interval/discuss/21602/Short-and-straight-forward-Java-solution

--- no18/src/Solution.php
+++ no18/src/Solution.php
@@ -4,4 +4,47 @@ namespace no18\src;
 class Solution
 {
 
+    /**
+     * @param Integer[][] $intervals
+     * @param Integer[] $newInterval
+     * @return Integer[][]
+     */
+    function insert($intervals, $newInterval) {
+
+        $results=[];
+        $currentIndex=0;
+        $intervalCount=count($intervals);
+
+        // just add the interval without overlapping
+        while($currentIndex<$intervalCount &&  $newInterval[0]> $intervals[$currentIndex][1]){
+            $results[]=$intervals[$currentIndex++];
+        }
+
+        // start merge
+        while($currentIndex<$intervalCount
+            && $newInterval[0]<= $intervals[$currentIndex][1]
+            && $newInterval[1]>=$intervals[$currentIndex][0]
+        ){
+
+            $newInterval[0]=min($intervals[$currentIndex][0], $newInterval[0]);
+            $newInterval[1]=max($intervals[$currentIndex][1], $newInterval[1]);
+
+            $currentIndex++;
+
+        }
+
+        // add merge results
+        $results[]=$newInterval;
+
+        // merge rest
+        while($currentIndex<$intervalCount){
+            $results[]=$intervals[$currentIndex++];
+        }
+
+        return $results;
+
+
+    }
+
+
 }
--- no18/tests/SolutionTest.php
+++ no18/tests/SolutionTest.php
@@ -18,9 +18,32 @@ class SolutionTest extends TestCase
 
     public function test1(){
 
-        $this->assertEquals('',$this->solution);
+        $this->assertEquals([[1,5],[6,9]],$this->solution->insert([[1,3],[6,9]],[2,5] ));
 
     }
 
+    public function test2(){
+
+        $this->assertEquals([[1,2],[3,10],[12,16]],$this->solution->insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]));
+
+    }
+
+    public function test3(){
+
+            $this->assertEquals([[5,7]],$this->solution->insert([], [5,7]));
+
+    }
+
+    public function test4(){
+
+        $this->assertEquals([[1,5]],$this->solution->insert([[1,5]], [2,3]));
+
+    }
+
+    public function test5(){
+
+        $this->assertEquals([[1,7]],$this->solution->insert([[1,5]], [2,7]));
+
+    }
 
 }
