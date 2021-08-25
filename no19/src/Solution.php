4bd3fb3dc7dffd70d71e27dd5b0222bc4f3c578c
Ian Chiang <ianc@motom.me>
2021-08-25 22:14:28 +0800

    q19
    - math solution, need to be better

--- no19/src/Solution.php
+++ no19/src/Solution.php
@@ -3,5 +3,36 @@
 namespace no19\src;
 class Solution
 {
+    /**
+     * @param Integer $m
+     * @param Integer $n
+     * @return Integer
+     */
+    function uniquePaths($m, $n) {
 
+        // m = rows
+        // n = columns
+
+        $table=[[]];
+
+
+
+        for ($row=0 ; $row<$m;$row++) {
+
+            for ($col=0 ; $col<$n;$col++) {
+
+                if($row===0 || $col===0){
+                    $table[$row][$col]=1;
+                }else{
+                    $table[$row][$col] =$table[$row-1][$col]+$table[$row][$col-1];
+                }
+
+            }
+
+        }
+
+        return $table[$m-1][$n-1];
+
+
+    }
 }
--- no19/tests/SolutionTest.php
+++ no19/tests/SolutionTest.php
@@ -18,9 +18,26 @@ class SolutionTest extends TestCase
 
     public function test1(){
 
-        $this->assertEquals('',$this->solution);
+        $this->assertEquals(28,$this->solution->uniquePaths(7,3));
 
     }
 
+    public function test2(){
+
+        $this->assertEquals(6,$this->solution->uniquePaths(3,3));
+
+    }
+
+    public function test3(){
+
+        $this->assertEquals(28,$this->solution->uniquePaths(3,7));
+
+    }
+
+    public function test4(){
+
+        $this->assertEquals(6,$this->solution->uniquePaths(3,3));
+
+    }
 
 }
