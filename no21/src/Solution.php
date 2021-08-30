f1aebbad157b24aea0d283e80c4b81c6ced2f50a
Ian Chiang <ianc@motom.me>
2021-08-30 17:14:13 +0800

    q21
    - finish by https://leetcode.com/problems/set-matrix-zeroes/discuss/26014/Any-shorter-O(1)-space-solution

--- no21/src/Solution.php
+++ no21/src/Solution.php
@@ -3,5 +3,58 @@
 namespace no21\src;
 class Solution
 {
+    /**
+     * @param Integer[][] $matrix
+     * @return NULL
+     */
+    function setZeroes(&$matrix) {
 
+        $colCount=count($matrix[0]);
+        $rowCount=count($matrix);
+
+        $col0Is0=false;
+
+        // mark which starts of row / column is 0
+        for ($row=0 ; $row<$rowCount;$row++) {
+
+            if($matrix[$row][0]===0){
+                // A
+                $col0Is0=true;
+            }
+
+            for ($col=1 ; $col<$colCount;$col++) {
+
+                if($matrix[$row][$col]===0){
+
+                    // mark row or col's beginning as 0
+                    $matrix[$row][0]=0;
+                    $matrix[0][$col]=0;
+                }
+
+            }
+
+        }
+
+
+        // start to set 0 from part 1 result
+        for ($row=$rowCount-1 ; $row>=0;$row--) {
+
+            for ($col=$colCount-1 ; $col>=1;$col--) {
+
+                if($matrix[$row][0]===0 || $matrix[0][$col]===0){
+                    $matrix[$row][$col]=0;
+                }
+
+            }
+            if($col0Is0){
+                // A'
+                $matrix[$row][0]=0;
+                //$matrix[$row][$col]=0;
+            }
+
+        }
+
+            echo json_encode($matrix);
+
+    }
 }
--- no21/tests/SolutionTest.php
+++ no21/tests/SolutionTest.php
@@ -18,9 +18,18 @@ class SolutionTest extends TestCase
 
     public function test1(){
 
-        $this->assertEquals('',$this->solution);
+        $matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]];
+         $this->solution->setZeroes($matrix);
+        $this->assertEquals([[1,0,1],[0,0,0],[1,0,1]], $matrix);
 
     }
 
+    public function test2(){
+
+        $matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]];
+        $this->solution->setZeroes($matrix);
+        $this->assertEquals([[0,0,0,0],[0,4,5,0],[0,3,1,0]],$matrix);
+
+    }
 
 }
