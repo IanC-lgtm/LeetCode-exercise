a54d6c6d6a6deae8a3e4ded32c9010396fdb285c
Ian Chiang <ianc@motom.me>
2021-08-30 15:27:03 +0800

    q20
    - init

---
+++ no20/tests/SolutionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace no20\tests;
+
+use no20\src\Solution;
+use PHPUnit\Framework\TestCase;
+
+class SolutionTest extends TestCase
+{
+
+    private $solution;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->solution = new Solution();
+    }
+
+    public function test1(){
+
+        $this->assertEquals('',$this->solution);
+
+    }
+
+
+}
