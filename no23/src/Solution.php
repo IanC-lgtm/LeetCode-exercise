4a0962b7491d7c2e0f2e5507299aa7447de6d061
Ian Chiang <ianc@motom.me>
2021-09-12 21:03:37 +0800

    q23
    - init

---
+++ no23/src/Solution.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace no23\src;
+class Solution
+{
+    /**
+     * @param String[][] $board
+     * @param String $word
+     * @return Boolean
+     */
+    function exist($board, $word) {
+
+    }
+}
---
+++ no23/tests/SolutionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace no23\tests;
+
+use no23\src\Solution;
+use PHPUnit\Framework\TestCase;
+
+class SolutionTest extends TestCase
+{
+
+    private $solution;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->solution = new Solution();
+    }
+
+    public function test1(){
+
+        $this->assertTrue(
+            $this->solution->exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],"ABCCED"));
+
+    }
+
+    public function test2(){
+
+        $this->assertTrue(
+            $this->solution->exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],"SEE"));
+
+    }
+
+    public function test3(){
+
+        $this->assertFalse(
+            $this->solution->exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],"ABCB"));
+
+    }
+
+}
