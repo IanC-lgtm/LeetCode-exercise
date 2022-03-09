0a5528101174536a745a924713169727d934e878
Ian Chiang <ianc@motom.me>
2022-03-09 16:51:34 +0800

    q28-1-o
    - faster solution with one line recursive

--- no28/src/Solution.php
+++ no28/src/Solution.php
@@ -17,38 +17,7 @@ class Solution
             return 0;
         }
 
-        $currentLevel = 1;
-        $queue = [$root];
-
-        while (count($queue) > 0) {
-
-            $count = count($queue);
-            $hasNextLevel = false;
-
-            for ($i = 0; $i < $count; $i++) {
-
-                $currentNode = $queue[0];
-
-                if ($currentNode->left) {
-                    $hasNextLevel = true;
-                    $queue[] = $currentNode->left;
-                }
-
-                if ($currentNode->right) {
-                    $hasNextLevel = true;
-                    $queue[] = $currentNode->right;
-                }
-                
-                // remove current node from queue
-                array_shift($queue);
-            }
-
-            if ($hasNextLevel) {
-                $currentLevel++;
-            }
-
-        }
-        return $currentLevel;
+        return 1 + max($this->maxDepth($root->left), $this->maxDepth($root->right));
     }
 
 }
