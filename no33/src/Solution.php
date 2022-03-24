f5ca7f45cc10d2765512e8dd0ce23a67977f05a1
Ian Chiang <ianc@motom.me>
2022-03-24 15:19:53 +0800

    q33-1-x

--- no33/src/Solution.php
+++ no33/src/Solution.php
@@ -10,5 +10,26 @@ class Solution
     function longestConsecutive($nums)
     {
 
+        $numMap = [];
+
+        foreach ($nums as $num) {
+
+            $numMap[$num] = true;
+
+        }
+
+        $best = 0;
+        foreach ($nums as $x) {
+
+            if (!isset($numMap[$x - 1])) {
+                $y = $x + 1;
+                while (isset($numMap[$y])) {
+                    $y += 1;
+                }
+                $best = max($best, $y - $x);
+            }
+        }
+
+        return $best;
     }
 }
